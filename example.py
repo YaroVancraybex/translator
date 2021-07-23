808512fb6aaa479adb616311b4712c51e9145d57
yaroaxito <yvlog123456789@gmail.com>
2021-07-23 12:49:27 +0200

    Example update

--- example.py
+++ example.py
@@ -1,4 +1,5 @@
 # This example will show you how the translation module can be implemented
 import translations as tr
-print(tr.cTranslate('The result is','de'))
+
+print(tr.cTranslate('Het resultaat is','de'))
 print(tr.languageDetect('The result is'))
